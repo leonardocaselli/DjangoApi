a765f29c7019bda00ec2c452ab149f772574966a
caun94 <cristobalugarte6@gmail.com>
2021-01-18 19:52:44 -0300

    caun94

--- gitproyect/app/views.py
+++ gitproyect/app/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render,HttpResponse,redirect
+from app.models import Movie
+from django.views.decorators.csrf import csrf_exempt,requires_csrf_token
 
+@csrf_exempt
+@requires_csrf_token
 def index(request):
-    return HttpResponse("Hola Mundo!! :)")
+	if request.method == "POST":
+		print(1)
+		a = Movie(title = "Divide", description = "Ed Sheeran", release_date = "2012-12-12" , duration = 20)
+		a.save()
+		return redirect("/request")
+
+	return HttpResponse("Hola Mundo!! :)")
